450b4eee7c2b5cb21d7cf259c4c1b934c579799d
App Academy Student <student@appacademy.io>
2015-07-10 14:57:36 -0700

    improve specs to test stackable where and not throw up false negatives

--- spec/02_searchable_spec.rb
+++ spec/02_searchable_spec.rb
@@ -16,34 +16,41 @@ describe 'Searchable' do
     end
   end
 
-  it '#where searches with single criterion' do
-    cats = Cat.where(name: 'Breakfast')
-    cat = cats.first
+  describe '#where' do
+    it 'searches with single criterion' do
+      cats = Cat.where(name: 'Breakfast')
+      cat = cats.first
 
-    expect(cats.length).to eq(1)
-    expect(cat.name).to eq('Breakfast')
-  end
+      expect(cats.length).to eq(1)
+      expect(cat.name).to eq('Breakfast')
+    end
 
-  it '#where can return multiple objects' do
-    humans = Human.where(house_id: 1)
-    expect(humans.length).to eq(2)
-  end
+    it 'can return multiple objects' do
+      humans = Human.where(house_id: 1)
+      expect(humans.length).to eq(2)
+    end
 
-  it '#where searches with multiple criteria' do
-    humans = Human.where(fname: 'Matt', house_id: 1)
-    expect(humans.length).to eq(1)
+    it 'searches with multiple criteria' do
+      humans = Human.where(fname: 'Matt', house_id: 1)
+      expect(humans.length).to eq(1)
 
-    human = humans[0]
-    expect(human.fname).to eq('Matt')
-    expect(human.house_id).to eq(1)
-  end
+      human = humans[0]
+      expect(human.fname).to eq('Matt')
+      expect(human.house_id).to eq(1)
+    end
 
-  it '#where returns [] if nothing matches the criteria' do
-    expect(Human.where(fname: 'Nowhere', lname: 'Man')).to eq([])
-  end
+    it 'comes up empty if nothing matches the criteria' do
+      expect(Human.where(fname: 'Nowhere', lname: 'Man')).to be_empty
+    end
 
-  it '#where stacks' do
-    matts = Human.where(fname: 'Matt').where(house_id: 1)
-    expect(matts.first.lname).to eq("Rubens")
+    it 'stacks' do
+      haskells = Cat.where(owner_id: 3).where(name: 'Haskell')
+      expect(haskells.first.id).to eq(3)
+    end
+
+    it 'double-stacks' do
+      matts = Human.where(fname: 'Matt').where(house_id: 1).where(id: 2)
+      expect(matts.first.lname).to eq("Rubens")
+    end
   end
 end
