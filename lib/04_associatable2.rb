600ffb000953816d22b2525dfbd263d6742db78e
Erez Bosch <erezaka@gmail.com>
2015-07-10 16:10:41 -0700

    add belongs_to => has_many handling to has_many_through

--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -32,7 +32,12 @@ module Associatable
     end
   end
 
-  # MANY THROUGH MANY
+  # SCENARIOS
+  # many => many: house has many humans which have many cats
+  # belongs => many: cat belongs to human which has dogs
+  # many => belongs: house has many cats which belong to human
+  # 1 down
+  # 2...
 
   def has_many_through(name, through_name, source_name)
     define_method(name) do
@@ -46,22 +51,46 @@ module Associatable
       source_f_key = source_options.foreign_key
       source_p_key = source_options.primary_key
 
-      p_key_value = send(through_options.primary_key)
+      through_many = through_options.instance_of? HasManyOptions
+      source_many = source_options.instance_of? HasManyOptions
 
-      results = DBConnection.execute(<<-SQL, p_key_value)
-        SELECT
-          #{source_table}.*
-        FROM
-          #{through_table}
-        JOIN
-          #{source_table}
-        ON
-          #{through_table}.#{source_p_key} = #{source_table}.#{source_f_key}
-        WHERE
-          #{through_table}.#{through_f_key} = ?
-      SQL
+      if through_many && source_many
+        p_key_value = send(through_options.primary_key)
+
+        results = DBConnection.execute(<<-SQL, p_key_value)
+          SELECT
+            #{source_table}.*
+          FROM
+            #{through_table}
+          JOIN
+            #{source_table}
+          ON
+            #{through_table}.#{source_p_key} = #{source_table}.#{source_f_key}
+          WHERE
+            #{through_table}.#{through_f_key} = ?
+        SQL
+
+      elsif source_many
+        through_p_key = through_options.primary_key
+        f_key_value = send(through_options.foreign_key)
+
+        results = DBConnection.execute(<<-SQL, f_key_value)
+          SELECT
+            #{source_table}.*
+          FROM
+            #{through_table}
+          JOIN
+            #{source_table}
+          ON
+            #{through_table}.#{through_p_key} = #{source_table}.#{source_f_key}
+          WHERE
+            #{through_table}.#{through_p_key} = ?
+        SQL
+      end
 
       results.map { |result| source_options.model_class.new(result) }
     end
   end
+
+  # def execute_sql_query(params)
 end
