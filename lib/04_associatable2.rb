b81d8785d2f40d0ca46e8327fb155c201a19e98b
App Academy Student <student@appacademy.io>
2015-07-10 20:08:56 -0700

    fix

--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -37,7 +37,9 @@ module Associatable
   # belongs => many: cat belongs to human which has dogs
   # many => belongs: house has many cats which belong to human
   # 1 down
-  # 2...
+  # 2 down
+  # 3...
+  # select * from 
 
   def has_many_through(name, through_name, source_name)
     define_method(name) do
@@ -46,51 +48,48 @@ module Associatable
 
       through_table = through_options.model_class.table_name
       through_f_key = through_options.foreign_key
+      through_p_key = through_options.primary_key
 
       source_table = source_options.model_class.table_name
       source_f_key = source_options.foreign_key
       source_p_key = source_options.primary_key
 
+      params = { one: source_table, two: through_table }
+
       through_many = through_options.instance_of? HasManyOptions
       source_many = source_options.instance_of? HasManyOptions
 
       if through_many && source_many
-        p_key_value = send(through_options.primary_key)
-
-        results = DBConnection.execute(<<-SQL, p_key_value)
-          SELECT
-            #{source_table}.*
-          FROM
-            #{through_table}
-          JOIN
-            #{source_table}
-          ON
-            #{through_table}.#{source_p_key} = #{source_table}.#{source_f_key}
-          WHERE
-            #{through_table}.#{through_f_key} = ?
-        SQL
-
+        params[:one_k] = source_f_key
+        params[:two_k] = source_p_key
+        params[:three_k] = through_f_key
+        value = send(through_options.primary_key)
       elsif source_many
-        through_p_key = through_options.primary_key
-        f_key_value = send(through_options.foreign_key)
+        params[:one_k] = source_f_key
+        params[:two_k] = through_p_key
+        params[:three_k] = through_p_key
+        value = send(through_options.foreign_key)
+      else #through_many
 
-        results = DBConnection.execute(<<-SQL, f_key_value)
-          SELECT
-            #{source_table}.*
-          FROM
-            #{through_table}
-          JOIN
-            #{source_table}
-          ON
-            #{through_table}.#{through_p_key} = #{source_table}.#{source_f_key}
-          WHERE
-            #{through_table}.#{through_p_key} = ?
-        SQL
-      end
+
+      results = self.class.execute_sql_query(params, value)
 
       results.map { |result| source_options.model_class.new(result) }
     end
   end
 
-  # def execute_sql_query(params)
+  def execute_sql_query(params, value)
+    DBConnection.execute(<<-SQL, value)
+      SELECT
+        #{params[:one]}.*
+      FROM
+        #{params[:two]}
+      JOIN
+        #{params[:one]}
+      ON
+        #{params[:one]}.#{params[:one_k]} = #{params[:two]}.#{params[:two_k]}
+      WHERE
+        #{params[:two]}.#{params[:three_k]} = ?
+    SQL
+  end
 end
