1e63c02d3aba2b976dc3d42eb8a983222847467b
Erez Bosch <erezaka@gmail.com>
2015-07-22 15:39:36 -0700

    refactor

--- lib/searchable.rb
+++ lib/searchable.rb
@@ -17,6 +17,43 @@ module Searchable
     Relation.new(self).where(params)
   end
 
+  def find_by(search_str)
+    data = DBConnection.execute(<<-SQL)
+    SELECT
+      *
+    FROM
+      #{table_name}
+    WHERE
+      #{search_str}
+    SQL
+
+    data.empty? ? nil : parse_all(data)
+  end
+
+  def method_missing(method_name, *args)
+    if method_name[0..7] == "find_by_"
+      cols = method_name[8..-1].split("_and_")
+      raise "You can't search by that" unless check_ivar?(cols)
+      find_by(make_search_str(cols,args))
+    end
+  end
+
+  def check_ivar?(cols)
+    cols.all? { |col| columns.include?(col) }
+  end
+
+  def make_search_str(cols, values)
+    search_arr = []
+    cols.length.times do |idx|
+      if values[idx].is_a?(Fixnum)
+        search_arr << "#{cols[idx]} = #{values[idx]}"
+      else
+        search_arr << "#{cols[idx]} = '#{values[idx]}'"
+      end
+    end
+    search_str = search_arr.join(" AND ")
+  end
+
   class Relation < BasicObject
     def initialize(obj_class)
       @klass = obj_class
